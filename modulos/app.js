2613ffd8566bb0e1d3cd8a83f3f22cfd9de9a1fd
Esteban Benito <ebenito@outlook.com>
2020-06-10 09:53:47 +0200

    Modulos

---
+++ modulos/app.js
@@ -0,0 +1,16 @@
+// Vamos a crear un fichero app.js que requerirá de un
+// módulo Triple, contendrá una función que triplica el valor que le
+// pasamos por parámetros a app.js
+//
+// Ejemplo:
+//   $ node common/app.js 1 // Resultado 3
+//   $ node common/app.js 2 // Resultado 6
+//   $ node common/app.js 4 // Resultado 12
+
+var varArgs = process.argv.slice(2);
+var num = parseInt(varArgs[0]);
+//console.log('El triple de', num, 'es', 3 * num);
+
+var tripleNumber = require('./triple.js');
+var result = tripleNumber(num);
+console.log(result);
