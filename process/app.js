170673a29093d64d15a501cedf2d7e0b7ff7c096
Esteban Benito <ebenito@outlook.com>
2020-06-10 20:38:32 +0200

    Process

---
+++ process/app.js
@@ -0,0 +1,24 @@
+"use strict" //A partir de ECMAScript versión 5.Con el modo estricto, no se pueden, por ejemplo, utilizar variables no declaradas.
+
+console.log('**************************************');
+//saco toda la info del proceso
+//console.log(process);
+
+console.log('Ejecutando en ' + process.env.OS + " con plataforma " + process.platform + ' y arquitectura ' + process.arch);
+
+console.log('Nombre de tu equipo: ' + process.env.COMPUTERNAME + " con procesador " + process.env.PROCESSOR_IDENTIFIER);
+
+console.log('**************************************');
+
+//version de node
+console.log('Versiónd de node.js: ' + process.version);
+
+console.log('Memoria usada', process.memoryUsage());
+
+console.log('Process ID (PID): ' + process.pid);
+
+console.log('Tiempo ejecuccion del script: ', process.uptime());
+
+process.exit(0);
+
+console.log("Este código ya no se ejecuta");
